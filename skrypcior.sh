98aed789df507447ee93ffe6ad98bd676ddbddef
KacperM45440 <dsw45440@student.dsw.edu.pl>
2022-05-11 19:42:58 +0200

    podpunkt3

--- skrypcior.sh
+++ skrypcior.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
-#asdadsad
 DATE()
 {
 data=$(date)
 echo $data;
 }
 
+LOGS()
+{
+for i in {1..100..1}
+do
+echo $(date) >> log${i}.txt
+echo $0 >> log${i}.txt
+done
+}
+
 if [ "$1" == "--date" ]
 then
 DATE
+fi
+
+if [ "$1" == "--logs" ]
+then
+LOGS
 fi
