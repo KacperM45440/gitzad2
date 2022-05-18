35034119015ee5188aef14c1e89465780c8d28fe
KacperM45440 <dsw45440@student.dsw.edu.pl>
2022-05-18 02:56:35 +0200

    zadanie6 ale zapomnialem zmienic brancha

--- skrypcior.sh
+++ skrypcior.sh
@@ -2,7 +2,8 @@
 i=1
 pierwszy=$1
 drugi=$2
-
+#SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+CURRENT_DIR=$(pwd)
 LOGS()
 {
 echo $pierwszy
@@ -22,26 +23,39 @@ data=$(date)
 echo $data
 }
 
-if [ "$1" == "--date" ]
+if [ "$1" == "--date" ] || [ "$1" == "-d" ]
 then
 DATE
 fi
 
-if [ "$1" == "--logs" ]
+if [ "$1" == "--logs" ] || [ "$1" == "-l" ]
 then
 LOGS
 fi
 
-if [ "$1" == "--help" ]
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]
 then
 echo "Dostepne opcje:"
-echo "--date | Wyswietla obecna date."
-echo "--logs [ilosc] | Tworzy logi."
-echo "--help | Wyswietla to menu."
+echo "--date/-d | Wyswietla obecna date."
+echo "--logs/-l [ilosc] | Tworzy logi."
+echo "--help/-h | Wyswietla to menu."
+echo "--init/-i | Klonuje repozytorium i robi cuda na patyku."
 fi
 
 if [ "$1" == "" ]
 then
 echo "Blad: Nie podano argumentu."
 echo "Wpisz --help aby zobaczyc dostepne argumenty, lub sprobuj jeszcze raz."
+fi
+
+if [ "$1" == "--init" ] || [ "$1" == "-i" ]
+then
+#echo $CURRENT_DIR
+#echo $SCRIPT_DIR
+git clone https://github.com/KacperM45440/gitzad2 $CURRENT_DIR
+export PATH="$PATH:$CURRENT_DIR"
+#TA ZMIANA WYKONUJE SIE TYLKO LOKALNIE, TJ.: NA CZAS WYKONYWANIA SKRYPTU.
+#CHCIALBYM UMIEC ZMIENIC SCIEZKE GLOBALNIE, ACZKOLWIEK GIT UPIERA SIE ZE NIE MA POTRZEBNYCH
+#DO TEGO PLIKOW, A JA SIE Z NIM O 3 W NOCY KLOCIC NIE BEDE:)
+echo "${PATH//:/$'\n'}"
 fi
